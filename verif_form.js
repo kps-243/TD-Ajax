697d0bca02761386c74ecf0467d256798f6ef9c9
Morgan Kpassi <morgan.kpassi@gmail.com>
2022-11-30 18:33:26 +0100

    Debut de la question 1

--- verif_form.js
+++ verif_form.js
@@ -1,1 +1,11 @@
-let a = $("#jeune").val();
+let errorJeune = document.getElementById("jeune_err");
+let errorVieux = document.getElementById("vieux_err");
+let jeune = document.getElementById("jeune");
+let vieux = document.getElementById("vieux");
+
+if ((jeune || vieux, 0 > jeune || vieux, jeune || vieux > 120)) {
+  document.getElementsById("jeune_err");
+  document.getElementsById("jeune_err");
+  document.textContent = "Veuillez écrire votre âge";
+} else {
+}
